71ebf3e0fd86506e3ce24936d48531e779410fa1
Sagittarius87 <Jarik-87@yandex.ru>
2024-04-24 12:22:53 +0300

    В ScoreLabel.js добавил bitmapText вместо text. В Game.js переписал метод createScoreLabel().

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -39,7 +39,7 @@ export class Game extends Scene
     createGameMessage(x, y) 
     {   
         const nameFont = 'PixeloidSans38';
-        const message = ['Как думаете, о чем сейчас мечтает котик?'];
+        const message = ['Как думаете, о чем сейчас\n мечтает котик?'];
         //const style = { fontFamily: 'PixeloidSans', fontSize: '38px', fill: '#ffffff' };
         const newMessage = new GameMessage(this, x, y, nameFont, message[0]);      
         return newMessage;
@@ -47,8 +47,9 @@ export class Game extends Scene
 
     createScoreLabel(x, y)
     {
-        const style = { fontFamily: 'PixeloidSans', fontSize: '38px', fill: '#ffffff' };
-        const newScore = new ScoreLabel(this, x, y, 0, style);
+        const nameFont = 'PixeloidSans38';
+        //const style = { fontFamily: 'PixeloidSans', fontSize: '38px', fill: '#ffffff' };
+        const newScore = new ScoreLabel(this, x, y, nameFont, 0);
         return newScore;
     }
 }
--- src/scenes/ScoreLabel.js
+++ src/scenes/ScoreLabel.js
@@ -5,18 +5,21 @@ const formatScore = (score) => {
 }
 
 export class ScoreLabel extends Phaser.GameObjects.Text
-
 {
-    constructor(scene, x, y, score, style)
+    nameFont;
+    score;
+
+    constructor(scene, x, y, nameFont, score)
     {
         super(scene, x, y);
 
         this.scene = scene;
         this.x = x;
         this.y = y;
+        this.nameFont = nameFont;
         this.score = score;
-        this.style = style;
-
-        this.score = scene.add.text(this.x, this.y, formatScore(this.score), this.style).setOrigin(0.5);
+        
+        //this.score = scene.add.text(this.x, this.y, formatScore(this.score), this.style).setOrigin(0.5);
+        this.score = scene.add.bitmapText(this.x, this.y, this.nameFont, formatScore(this.score)).setOrigin(0.5);
     }
 }
